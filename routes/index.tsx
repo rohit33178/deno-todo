74ab7316044ea3f16da6d9646188409703c12cb8
@rohit.singh <rohit.singh@firstprinciples.io>
2024-10-19 20:16:03 +0530

    update the layout reduce height

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,16 +4,16 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-4 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
-          class="my-6"
+          class="my-4"
           src="/logo.svg"
-          width="128"
-          height="128"
+          width="64"
+          height="64"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Deno Todo App</h1>
+        <h1 class="text-4xl font-bold">Deno Todo App</h1>
         <Counter count={count} />
       </div>
     </div>
